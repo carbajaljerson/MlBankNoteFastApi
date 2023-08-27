5d59666d387a701fee61ea8ad1140eef7fc01453
Jerson Carbajal <jerson182@hotmail.com>
2023-08-26 19:10:40 -0500

    commit

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def predict_banknote(data:BankNote):
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(appFastApi, host='127.0.0.1', port=8000)
+#if __name__ == '__main__':
+#    uvicorn.run(appFastApi, host='127.0.0.1', port=8000)
     
 #uvicorn app:appFastApi --reload
